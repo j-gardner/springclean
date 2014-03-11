861cd1c3e2df1c683db9b5d83d89873518ec479c
John Gardner <john@arconixpc.com>
2014-03-11 00:04:15 -0400

    rename menu.js to better reflect the file's purpose

---
+++ includes/js/script.js
@@ -0,0 +1,11 @@
+jQuery(document).ready( function(){
+
+    jQuery( '.genesis-nav-menu' ).addClass( 'header-menu' ).before( '<div id="header-menu-icon" class="fa fa-bars fa-lg"></div>' );
+
+    jQuery( '#header-menu-icon' ).click( function() {
+        jQuery( '.genesis-nav-menu' ).fadeToggle();
+    } );
+
+    jQuery( '.entry-content ul' ).addClass( 'fa-ul' );
+
+});
